15a8da9e1d66757f32fdd615b0b6949b8fcf9af2
Muskan-Malhotra <muskan01malhotra@gmail.com>
2024-08-26 14:00:00 +0100

    The message

--- hello.java
+++ hello.java
@@ -5,7 +5,6 @@ public class hello {
 
     public static void main(String[] args){
         System.out.println("Hello World!");
-        System.out.println("Hello People!");
         //this is it making it good! 
     }
 }
