77e2d107eb59fe1496578a8200989824697e0f22
Muskan-Malhotra <muskan01malhotra@gmail.com>
2024-08-29 23:51:37 +0530

    16 gfg find length of loop

---
+++ _16_GFG_Find_length_of_loop.java
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class _16_GFG_Find_length_of_loop {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void makeLoop(Node head, Node tail, int x) {
+        if (x == 0) return;
+
+        Node curr = head;
+        for (int i = 1; i < x; i++) curr = curr.next;
+
+        tail.next = curr;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+            int k = Integer.parseInt(br.readLine());
+            Node head = new Node(arr.get(0));
+            Node tail = head;
+            for (int i = 1; i < arr.size(); ++i) {
+                tail.next = new Node(arr.get(i));
+                tail = tail.next;
+            }
+            makeLoop(head, tail, k);
+
+            Solution ob = new Solution();
+            System.out.println(ob.countNodesinLoop(head));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+*/
+
+// Function should return the length of the loop in LL.
+
+class Solution {
+    // Function to find the length of a loop in the linked list.
+    public int countNodesinLoop(Node head) {
+        // Add your code here.
+        
+        Node slow_p = head, fast_p = head;
+ 
+        while (slow_p != null && fast_p != null
+               && fast_p.next != null) {
+            slow_p = slow_p.next;
+            fast_p = fast_p.next.next;
+ 
+            /* If slow_p and fast_p meet at some point
+            then there is a loop */
+            if (slow_p == fast_p)
+                return countNodes(slow_p);
+        }
+ 
+        /* Return 0 to indicate that there is no loop*/
+        return 0;
+    }
+    
+    int countNodes(Node n)
+    {
+        int res = 1;
+        Node temp = n;
+        while (temp.next != n) {
+            res++;
+            temp = temp.next;
+        }
+        return res;
+    }
+}
