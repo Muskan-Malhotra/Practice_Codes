091b8485383b3c6bbdc31cc50cf9d55ac505f616
Muskan-Malhotra <muskan01malhotra@gmail.com>
2024-09-06 23:43:09 +0530

    24 gfg kadanes algorithm

---
+++ _24_gfg_kadanes_algo.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class _24_gfg_kadanes_algo {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+
+            // calling maxSubarraySum() function
+            System.out.println(obj.maxSubarraySum(arr));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+
+    // arr: input array
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int[] arr) {
+
+        // Your code here
+        int max_so_far = Integer.MIN_VALUE;
+        int max_ending = 0;
+        
+        for(int i=0;i<arr.length;i++){
+            max_ending += arr[i];
+            
+            if(max_so_far < max_ending){
+                max_so_far = max_ending;
+            } if (max_ending < 0) {
+                max_ending = 0;
+            }
+        }
+        
+        return max_so_far;
+    }
+}
+
