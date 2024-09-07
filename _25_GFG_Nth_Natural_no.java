b215e2a7c9644c60f7fb0feebe709dae5a8a62b7
Muskan-Malhotra <muskan01malhotra@gmail.com>
2024-09-07 22:37:55 +0530

    25 GFG Nith Natural no

---
+++ _25_GFG_Nth_Natural_no.java
@@ -0,0 +1,50 @@
+
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class _25_GFG_Nth_Natural_no {
+    public static void main(String[] args) {
+        Scanner ob = new Scanner(System.in);
+        int t = ob.nextInt();
+        while (t-- > 0) {
+            long n = ob.nextLong();
+            Solution ab = new Solution();
+            long k = ab.findNth(n);
+            System.out.println(k);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    long findNth(long N) {
+        // Stores the Nth number
+    long result = 0;
+
+    long p = 1;
+
+    // Iterate while N is
+    // greater than 0
+    while (N > 0) {
+
+      // Update result
+      result += (p * (N % 9));
+
+      // Divide N by 9
+      N = N / 9;
+
+      // Multiply p by 10
+      p = p * 10;
+    }
+
+    // Return result
+    return result;
+    }
+}
